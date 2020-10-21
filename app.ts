d0d8a09107214fe4f3ea8c83a1e1353609f9870c
Hugh <det.nosnibor00@gmail.com>
2020-10-21 12:00:20 -0500

    alias types

--- app.ts
+++ app.ts
@@ -1,20 +1,26 @@
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-    input1: number | string, 
-    input2: number | string, 
-    resultConversion: 'as-number' | 'as-text') 
-    {
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
     result = +input1 + +input2;
   } else {
-      result = input1.toString() + input2.toString();
+    result = input1.toString() + input2.toString();
   }
   return result;
-//   if (resultConversion === 'as-number') {
-//       return +result;
-//   } else {
-//     return result.toString();
-//   }
+  //   if (resultConversion === 'as-number') {
+  //       return +result;
+  //   } else {
+  //     return result.toString();
+  //   }
 }
 
 const combineAges = combine(30, 26, 'as-number');
