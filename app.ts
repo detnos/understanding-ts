50a34b4df23d27157656afcbcfcfe201dfd71c2f
Hugh <det.nosnibor00@gmail.com>
2020-10-27 11:47:49 -0500

    never type

--- app.ts
+++ app.ts
@@ -1,4 +1,15 @@
 let userInput : unknown;
+let userName : string;
 
 userInput = 5;
 userInput = 'Max';
+// userName = userInput; //gives compile error because unknown type of userInput
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+function generateError(message: string, code: number) {
+    throw {message: message, errorCode: code}
+}
+
+generateError('An error occured!', 500);
