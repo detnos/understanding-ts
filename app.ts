b98148a9d40c515bf659262094a13af2f0eabd81
Hugh <det.nosnibor00@gmail.com>
2020-10-27 11:38:59 -0500

    additional function type info

--- app.ts
+++ app.ts
@@ -8,9 +8,18 @@ function printResult(num: number) {
 
 printResult(add(5, 12));
 
-let combinedValues: Function;
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result);
+}
+
+let combinedValues: (a: number, b: number) => number; //Function will accept any function () => number [takes any function that takes no parameters and returns a number]
 
 combinedValues = add;
 // combinedValues = 5; //runtime error even though TypeScript lets it compile w/o errors (combinedValues is type any so it allows the reassignment but later in the code we use combinedValues as a pointer to the function)
 
 console.log(combinedValues(8,8));
+
+addAndHandle(10, 20, (result)=>{
+    console.log(result)
+})
