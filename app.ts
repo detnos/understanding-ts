14e8b3939e326b0c40c3498ef59f6267e8beece1
Hugh <det.nosnibor00@gmail.com>
2020-10-27 11:32:47 -0500

    function types

--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ function printResult(num: number) {
 }
 
 printResult(add(5, 12));
+
+let combinedValues: Function;
+
+combinedValues = add;
+// combinedValues = 5; //runtime error even though TypeScript lets it compile w/o errors (combinedValues is type any so it allows the reassignment but later in the code we use combinedValues as a pointer to the function)
+
+console.log(combinedValues(8,8));
